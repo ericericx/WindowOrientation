5576ffd3769e485ac1984d5f0d62237f312a949c
Shinren Pan <shinren.pan@gmail.com>
2018-05-08 18:06:42 +0800

    Done

--- WindowOrientation/CustomWindow.m
+++ WindowOrientation/CustomWindow.m
@@ -37,7 +37,7 @@
         [self.btnWhite addTarget:self action:@selector(whitAction:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:self.btnWhite];
         
-//        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleOrientationChange:) name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleOrientationChange:) name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
     }
     return self;
 }
@@ -54,58 +54,37 @@
     if (!self.isUserInteractionEnabled || self.isHidden || self.alpha <= 0.01) {
         return nil;
     }
-    if ([self pointInside:point withEvent:event]) {
-        for (UIView *subview in [self.subviews reverseObjectEnumerator]) {
-            CGPoint convertedPoint = [subview convertPoint:point fromView:self];
-            UIView *hitTestView = [subview hitTest:convertedPoint withEvent:event];
-            NSLog(@"%@", hitTestView);
-            if (hitTestView) {
-                return hitTestView;
-            }
-        }
-        return self;
+    
+    if (CGRectContainsPoint(_btnWhite.frame, point))
+    {
+        return _btnWhite;
     }
+    
+    if (CGRectContainsPoint(_btnRed.frame, point))
+    {
+        return _btnRed;
+    }
+    
     return nil;
 }
 
-//- (void)handleOrientationChange:(NSNotification *)note
-//{
-//    [self bringSubviewToFront:self.btnRed];
-//    [self bringSubviewToFront:self.btnWhite];
-//
-//    UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
-//    //    [self setTransform:[self transformForOrientation:orientation]];
-//
-//    CGRect orientationFrame = [UIScreen mainScreen].bounds;
-//    CGRect statusBarFrame = [UIApplication sharedApplication].statusBarFrame;
-//
-//    if(UIInterfaceOrientationIsLandscape(orientation)) {
-//        float temp = orientationFrame.size.width;
-//        orientationFrame.size.width = orientationFrame.size.height;
-//        orientationFrame.size.height = temp;
-//
-//        temp = statusBarFrame.size.width;
-//        statusBarFrame.size.width = statusBarFrame.size.height;
-//        statusBarFrame.size.height = temp;
-//    }
-//
-//    self.frame = orientationFrame;
-//}
-//
-//- (CGAffineTransform) transformForOrientation:(UIInterfaceOrientation)orientation {
-//    switch (orientation) {
-//        case UIInterfaceOrientationLandscapeLeft:
-//            return CGAffineTransformMakeRotation(-DegreesToRadians(90));
-//        case UIInterfaceOrientationLandscapeRight:
-//            return CGAffineTransformMakeRotation(DegreesToRadians(90));
-//        case UIInterfaceOrientationPortraitUpsideDown:
-//            return CGAffineTransformMakeRotation(DegreesToRadians(180));
-//        case UIInterfaceOrientationPortrait:
-//        default:
-//            return CGAffineTransformMakeRotation(DegreesToRadians(0));
-//    }
-//}
+- (void)handleOrientationChange:(NSNotification *)note
+{
+    self.frame = [[UIScreen mainScreen]bounds];
+    [self __setupRedButtonFrame];
+    [self __setupWhiteButtonFrame];
+}
 
+- (void)__setupRedButtonFrame
+{
+    CGRect frame = self.bounds;
+    self.btnRed.frame = CGRectMake( 0, 0, frame.size.width, frame.size.height);
+}
 
+- (void)__setupWhiteButtonFrame
+{
+    CGRect frame = self.bounds;
+    self.btnWhite.frame = CGRectMake( 0, frame.size.height - (frame.size.height - frame.size.width - 1), frame.size.width, frame.size.height - frame.size.width - 1);
+}
 
 @end
